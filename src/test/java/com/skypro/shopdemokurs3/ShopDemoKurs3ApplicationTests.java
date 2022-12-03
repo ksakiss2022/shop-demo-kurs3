3e951c2e5e2c97208b8a0ff814c40d9d8e2475bb
Swetlana <sweta_zaika@mail.ru>
2022-12-03 23:35:44 +0300

    интернет магазин задание курса 3 Spring

--- src/test/java/com/skypro/shopdemokurs3/ShopDemoKurs3ApplicationTests.java
+++ src/test/java/com/skypro/shopdemokurs3/ShopDemoKurs3ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ShopDemoKurs3ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
